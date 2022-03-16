20a6113ebcb8a7e5646ae4ae7175c987113749dd
karami4yasser <karamiyassir2000@gmail.com>
2022-03-16 17:05:06 +0100

    'All'

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {Provider} from "react-redux"
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import tw from 'tailwind-react-native-classnames';
+import MapScreen from './screens/MapScreen';
+
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <Provider>
-    <View style={styles.container}>
-      <Text>Yasser karami</Text>
-      <StatusBar style="auto" />
-    </View></Provider>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+            style={tw`flex-1`}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
